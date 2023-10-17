8f57882ffc0d1fa7fa28f15357c280ced26282e1
qianfei11 <490584635@qq.com>
2023-10-17 10:03:34 +0800

    add testing for function-sections

--- linker-script/syscall_1.c
+++ linker-script/syscall_1.c
@@ -1,6 +1,9 @@
 #include "./syscall_1.h"
+#include "./user_ta.h"
 
 void syscall_1_write() {
     puts("calling syscall_1_write");
+    void *result = to_user_ta_ctx();
+    printf("result = %p\n", result);
 }
 
---
+++ linker-script/user_ta.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+static inline void *to_user_ta_ctx(void)
+{
+    void *result = NULL;
+    return result;
+}
